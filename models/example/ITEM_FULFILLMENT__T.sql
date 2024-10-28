cf0aa858712702e7bba17b031f59c111c38004a2
Eyal Diamant <data_admin@cellebrite.com>
2024-10-28 16:56:46 +0000

    Final before QA

---
+++ models/example/ITEM_FULFILLMENT__T.sql
@@ -0,0 +1,46 @@
+
+/*
+    Welcome to your first dbt model!
+    Did you know that you can also configure models directly within SQL files?
+    This will override configurations stated in dbt_project.yml
+
+    Try changing "table" to "view" below
+*/
+
+{{ config(
+    materialized='table') }}
+
+with TRANSACTION_LINES as 
+    (   select  TRANSACTION_ID 	            
+                , TRANSACTION_LINE_ID 	        as CREATED_FROM_LINE_ID
+                , ITEM_COUNT                
+        from    {{env_var('DBT_DATALAKE_DB')}}.NETSUITE.TRANSACTION_LINES
+        where   ACCOUNT_ID is not null 
+                and TAX_TYPE is null 
+                and ITEM_ID is not null 
+                and ITEM_ID <> '87'
+), 
+ORDER_STG as 
+    (   select distinct TRANID || ' | ' || TRANSACTION_LINE_ID  as _Line_Key 
+        from {{ ref('ORDER_STG') }}
+
+)
+select  TRN.TRANID  
+        , To_Date(TRN.TRANDATE)                                     as TRANDATE
+        , TRN.CREATED_FROM_ID AS _CreatedFromId_Key
+        , TRN.TRANSACTION_ID AS _ITFLineKey
+        , TRL.TRANSACTION_ID || ' | ' || TRL.CREATED_FROM_LINE_ID  as _Line_Key    
+        , ORD.*
+from    {{env_var('DBT_DATALAKE_DB')}}.NETSUITE.TRANSACTIONS        TRN
+        left outer join 
+        {{env_var('DBT_DATALAKE_DB')}}.NETSUITE.TRANSACTIONS        TRN1
+        on TRN.CREATED_FROM_ID = TRN1.TRANSACTION_ID
+        left outer join
+        TRANSACTION_LINES                                           TRL
+        on TRN1.TRANSACTION_ID = TRL.TRANSACTION_ID
+        left outer join 
+        ORDER_STG                                                   ORD
+        on _Line_Key = ORD._Line_Key
+Where   TRN.TRANSACTION_TYPE = 'Item Fulfillment' 
+
+
--- models/example/ORDER_STG.sql
+++ models/example/ORDER_STG.sql
@@ -11,9 +11,20 @@
     materialized='table',
     database='STAGING_DB_DEV') }}
 
-Select	TRANID
+with  TRANSACTION_LINES as 
+    ( select   TRANSACTION_ID
+                , TRANSACTION_LINE_ID
+	            , ITEM_ID AS _ItemKey
+        from    {{env_var('DBT_DATALAKE_DB')}}.NETSUITE.TRANSACTION_LINES
+        where   ACCOUNT_ID is not null 
+                and TAX_TYPE is null 
+                and ITEM_ID is not null   
+                and ITEM_ID <> '87'
+    ) 
+
+select	TRANID
         , ORDER_TYPE_ID
-        , TRANSACTION_ID
+        , TRN.TRANSACTION_ID
         , TOTAL_AMOUNT_
         , case when ORDER_TYPE_ID = 4 and TOTAL_AMOUNT_ <2
             then 0
@@ -25,9 +36,25 @@ Select	TRANID
             then 2 
             else 0 
             end                                     as ORDER_TYPE_CATEGORY
-        , TRANSACTION_ID                            as _OrderKey
+        , TRN.TRANSACTION_ID                        as _OrderKey
         , ENTITY_ID                                 as _CustomerKey 	
-from    {{env_var('DBT_DATALAKE_DB')}}.NETSUITE.TRANSACTIONS
+        , REPRESENTS_SUBSIDIARY_ID
+        , PRODUCT_FAMILY_ID
+        , TRANSACTION_LINE_ID
+from    {{env_var('DBT_DATALAKE_DB')}}.NETSUITE.TRANSACTIONS        TRN
+        left outer join 
+        TRANSACTION_LINES                                           TRL
+        on TRN.TRANSACTION_ID = TRL.TRANSACTION_ID
+        left outer join 
+        {{env_var('DBT_DATALAKE_DB')}}.NETSUITE.CUSTOMERS           CUS
+        on TRN.ENTITY_ID = CUS.CUSTOMER_ID
+       left outer join 
+        {{env_var('DBT_DATALAKE_DB')}}.NETSUITE.ITEMS               ITM
+        on TRL._ItemKey = ITM.ITEM_ID
 where    TRANSACTION_TYPE = 'Sales Order' 
         and NON_COMMERCIAL_INVOICE <> 'T' 
+        and REPRESENTS_SUBSIDIARY_ID is null 
+	    and IS_NOT_RMA = 1 
+        and ORDER_TYPE_CATEGORY in (1,2)    
+        and PRODUCT_FAMILY_ID <> '43'
       
